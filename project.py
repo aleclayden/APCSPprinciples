43e6efcbb6628f3689e864d45df6fa4a3bc78c16
aleclayden <57370899+aleclayden@users.noreply.github.com>
2019-11-04 11:55:16 -0800

    Add files via upload

---
+++ project.py
@@ -0,0 +1,24 @@
+import random
+
+randoNumber = random.randint(1000, 9999)
+print(randoNumber)
+
+for i in range(1):
+    userinput = input("Guess a Random Number between 1000 and 9999")
+    if int(userinput) != randoNumber:
+            print("Shocker, you are wrong")
+    elif int(userinput) == randoNumber:
+            print("Congratulations")
+
+booleanData = True
+
+while booleanData = True:
+
+    
+
+
+
+
+
+
+
